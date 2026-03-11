591a1a7e3bcd4ede7969d02c731e4a4c528edbfb
Rujorio <aluno27216@ipt.pt>
2026-03-11 10:06:52 +0000

    os moels

---
+++ API/API/Models/Compra.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Models
+{
+    public class Compra
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public State State { get; set; }
+    }
+    
+    public enum State
+    {
+        Pending,
+        Paid,
+        Sent,
+        DDelivered,
+        Closedd
+    }
+}
